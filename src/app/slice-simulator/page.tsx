c24e45168c7a0de789cc7f087d9ce0067970ad2d
Sandesh <sandeshkunwar.1337@gmail.com>
2024-04-13 21:28:10 +0000

    Refactor SliceSimulatorPage component to pass background color as a prop

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -13,8 +13,7 @@ export default function SliceSimulatorPage({
   const slices = getSlices(searchParams.state);
 
   return (
-    <SliceSimulator>
-      background:"#070815" ;
+    <SliceSimulator background="#070815"> {/* Note the use of curly braces to pass the background color as a prop */}
       <SliceZone slices={slices} components={components} />
     </SliceSimulator>
   );
