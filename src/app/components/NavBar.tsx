2ea4b8b6bfef632846007d3e3306bc145d388136
Sandesh <sandeshkunwar.1337@gmail.com>
2024-04-13 20:29:59 +0000

    Refactor NavBar component to include PrismicNextLink and improve accessibility

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -5,26 +5,28 @@ import { PrismicNextLink } from '@prismicio/next';
 import Link from 'next/link';
 
 type NavBarProps = {
-    settings: Content.SettingsDocument;
-    };
-export default function NavBar({settings}: NavBarProps) {
+  settings: Content.SettingsDocument;
+};
+export default function NavBar({ settings }: NavBarProps) {
   return (
-    <div>
+    <nav aria-label="Main">
       <Link href="/">
         <WordMark />
         <span className="sr-only">Hippes Central Home Page</span>
       </Link>
 
-      <nav aria-label ="Footer">
-        <ul className='flex gap-6'>
-          {settings.data.navigation.map((item) => (
-            <li key={item.label}>
-              <PrismicNextLink field={item.link}
-              className="inline-flex min-h-11 text-sm text-slate-400 hover:text-white">{item.label}</PrismicNextLink>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </div>
+      <ul className="flex gap-6">
+        {settings.data.navigation.map((item) => (
+          <li key={item.label}>
+            <PrismicNextLink
+              field={item.link}
+              className="inline-flex min-h-11 text-sm text-slate-400 hover:text-white"
+            >
+              {item.label}
+            </PrismicNextLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 }
