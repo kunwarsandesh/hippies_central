f316435278e2a39cb89f96ba1bd2650823afea33
Sandesh <sandeshkunwar.1337@gmail.com>
2024-04-13 20:33:47 +0000

    Refactor NavBar component to include PrismicNextLink, improve accessibility, and update styling

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -9,7 +9,8 @@ type NavBarProps = {
 };
 export default function NavBar({ settings }: NavBarProps) {
   return (
-    <nav aria-label="Main">
+    <nav className='px-4 py-4 md:px-6 md:py-6' aria-label="Main">
+        <div className='mx-auto flex max-w-6xl flex-col justify-between py-2 font-medium text-white md:flex-row md:items-center'>
       <Link href="/">
         <WordMark />
         <span className="sr-only">Hippes Central Home Page</span>
@@ -27,6 +28,7 @@ export default function NavBar({ settings }: NavBarProps) {
           </li>
         ))}
       </ul>
+      </div>
     </nav>
   );
 }
