551e13526600e1d5ee6da64430a71f49b0d22199
Sandesh <sandeshkunwar.1337@gmail.com>
2024-04-13 17:24:29 +0000

    Update Next.js configuration to include custom Sass include path

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+// Converting the file URL to a file path
+const __filename = fileURLToPath(import.meta.url);
+
+// Deriving the directory name
+const __dirname = path.dirname(__filename);
+
+// Next.js configuration object
+const nextConfig = {
+  sassOptions: {
+    // includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, '/src/styles')]
+  }
+};
+
+// Exporting the Next.js configuration object
 export default nextConfig;
