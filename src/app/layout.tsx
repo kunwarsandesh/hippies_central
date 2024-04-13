746526fd8359a747e2a408928ef37eb47a13485a
Sandesh <sandeshkunwar.1337@gmail.com>
2024-04-13 17:19:24 +0000

    Update font import and add global.scss for dark mode support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,10 @@ import { PrismicPreview } from '@prismicio/next';
 import { repositoryName } from '@/prismicio';
 import { DM_Sans } from 'next/font/google';
 
-const dm_sans = DM_Sans({
+const dmSans = DM_Sans({
   subsets: ['latin'],
-  display: 'swap'
+  display: 'swap',
+  variable: '--font-dm-sans'
 });
 
 export default function RootLayout({
