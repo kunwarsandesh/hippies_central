6dcb27096e529658c3da82f16af062ed9668be54
Sandesh <sandeshkunwar.1337@gmail.com>
2024-04-14 23:17:01 +0000

    Refactor CaseStudies slice component to include PrismicNextImage and clsx in src/slices/CaseStudies/index.tsx

--- src/slices/CaseStudies/index.tsx
+++ src/slices/CaseStudies/index.tsx
@@ -1,11 +1,12 @@
 import { createClient } from '@/prismicio';
 import { Content, isFilled } from '@prismicio/client';
-import { PrismicNextLink } from '@prismicio/next';
+import { PrismicNextImage, PrismicNextLink } from '@prismicio/next';
 import {
   PrismicRichText,
   PrismicText,
   SliceComponentProps
 } from '@prismicio/react';
+import clsx from 'clsx';
 
 /**
  * Props for `CaseStudies`.
@@ -46,20 +47,31 @@ const CaseStudies = async ({
                 key={caseStudy.id}
                 className="relative grid gap-4 opacity-85 transition-opacity duration-300 hover:cursor-pointer hover:opacity-100 md:grid-cols-2 md:gap-8 lg:grid-cols-3"
               >
-                <h3 className="text-4xl">
-                  <PrismicText field={caseStudy.data.company} />
-                </h3>
-                <div className="max-w-md">
-                  <PrismicRichText field={caseStudy.data.description} />
-                </div>
+                <div className="col-span-1 flex flex-col justify-center">
+                  <h3 className="text-4xl">
+                    <PrismicText field={caseStudy.data.company} />
+                  </h3>
+                  <div className="max-w-md">
+                    <PrismicRichText field={caseStudy.data.description} />
+                  </div>
 
-                <PrismicNextLink
-                  document={caseStudy}
-                  className="after:absolute after:inset-0 hover:underline"
-                >
-                  Read <PrismicText field={caseStudy.data.company} />
-                  &apos;s case study
-                </PrismicNextLink>
+                  <PrismicNextLink
+                    document={caseStudy}
+                    className="after:absolute after:inset-0 hover:underline"
+                  >
+                    Read <PrismicText field={caseStudy.data.company} />
+                    &apos;s case study
+                  </PrismicNextLink>
+                </div>
+                <PrismicNextImage
+                  field={caseStudy.data.logo_image}
+                  quality={100}
+                  // alt={`Logo for ${caseStudy.data.company}`}
+                  className={clsx(
+                    'rounded-xl lg:col-span-2',
+                    index % 2 && 'md:-order-1'
+                  )}
+                />
               </div>
             );
           }
