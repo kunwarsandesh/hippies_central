e1e76ac4e6ccd46e4edf8cac9f8e2406226c6880
Sandesh <sandeshkunwar.1337@gmail.com>
2024-04-14 12:24:39 +0000

    Update Bento slice component to include PrismicNextImage and PrismicRichText imports

--- src/slices/Bento/index.tsx
+++ src/slices/Bento/index.tsx
@@ -1,6 +1,9 @@
+import Bounded from '@/app/components/Bounded';
 import { Content, asText } from '@prismicio/client';
 import { PrismicNextImage } from '@prismicio/next';
 import { PrismicRichText, SliceComponentProps } from '@prismicio/react';
+import { components } from '..';
+import { ChildProcess } from 'child_process';
 
 /**
  * Props for `Bento`.
@@ -12,11 +15,25 @@ export type BentoProps = SliceComponentProps<Content.BentoSlice>;
  */
 const Bento = ({ slice }: BentoProps): JSX.Element => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <PrismicRichText field={slice.primary.heading} />
+      <PrismicRichText
+        field={slice.primary.heading}
+        components={{
+          heading2: ({ children }) => (
+            <h2 className="text-balance text-center text-5xl font-medium md:text-7xl">
+              {children}
+            </h2>
+          ),
+          em: ({ children }) => (
+            <em className="bg-gradient-to-b from-yellow-100 to-yellow-500 bg-clip-text not-italic text-transparent">
+              {children}
+            </em>
+          )
+        }}
+      />
       <PrismicRichText field={slice.primary.body} />
       {slice.items.map((item) => (
         <div
@@ -28,7 +45,7 @@ const Bento = ({ slice }: BentoProps): JSX.Element => {
           <PrismicNextImage field={item.image} />
         </div>
       ))}
-    </section>
+    </Bounded>
   );
 };
 
