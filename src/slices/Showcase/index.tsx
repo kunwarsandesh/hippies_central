d5724f428aabb54d24778811b16872ff1a640912
Sandesh <sandeshkunwar.1337@gmail.com>
2024-04-14 18:14:45 +0000

    Refactor Showcase slice component to improve layout and styling in src/slices/Showcase/index.tsx

--- src/slices/Showcase/index.tsx
+++ src/slices/Showcase/index.tsx
@@ -16,15 +16,31 @@ const Showcase = ({ slice }: ShowcaseProps): JSX.Element => {
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="relative"
     >
-      <PrismicRichText field={slice.primary.heading} />
-      <PrismicNextImage field={slice.primary.image} />
-      <>{slice.primary.icon}</>
-      <PrismicRichText field={slice.primary.subheading} />
-      <PrismicRichText field={slice.primary.body} />
-      <PrismicNextLink field={slice.primary.button_link}>
-        {slice.primary.button_text}
-      </PrismicNextLink>
+      <div className="glow absolute -z-10 aspect-square w-full max-w-xl rounded-full bg-blue-400/20 blur-3xl filter" />
+
+      <PrismicRichText
+        field={slice.primary.heading}
+        components={{
+          heading2: ({ children }) => (
+            <h2 className="text-balance text-center text-5xl font-medium md:text-7xl">
+              {children}
+            </h2>
+          )
+        }}
+      />
+      <div className="grid">
+        <div>
+          <>{slice.primary.icon}</>
+          <PrismicRichText field={slice.primary.subheading} />
+          <PrismicRichText field={slice.primary.body} />
+          <PrismicNextLink field={slice.primary.button_link}>
+            {slice.primary.button_text}
+          </PrismicNextLink>
+        </div>
+        <PrismicNextImage field={slice.primary.image} />4
+      </div>
     </Bounded>
   );
 };
