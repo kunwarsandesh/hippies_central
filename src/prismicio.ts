c0695dc84f2d8c89c71b0ba70dab0b61ea041ec6
Sandesh <sandeshkunwar.1337@gmail.com>
2024-04-15 19:38:48 +0000

    Add new route for case studies in prismicio.ts

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -13,7 +13,8 @@ export const repositoryName =
  */
 const routes: prismic.ClientConfig['routes'] = [
   { type: 'page', path: '/', uid: 'home' },
-  { type: 'page', path: '/:uid' }
+  { type: 'page', path: '/:uid' },
+  { type: 'case_study', path: '/case-study/:uid' }
 ];
 
 /**
